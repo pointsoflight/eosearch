d1ec9b2d39a1de7a81f291d0bda7241bc587d779
Charles Julian Knight <charles@rabidaudio.com>
2016-06-04 11:35:25 -0400

    endpoint for ein

--- index.js
+++ index.js
@@ -10,21 +10,49 @@ pg.connect(conString, function(err, client, done) {
     return console.error('error fetching client from pool', err);
   }
 
-  app.get('/', function(req, res){
+  // FIND: /api/records/:EIN
+  app.get('/api/records/:ein', function(req, res){
 
-    client.query('SELECT * from irs_eo_records;', function(err, result) {
-      //call `done()` to release the client back to the pool
+    var ein = req.params.ein;
+
+    // todo find
+
+    client.query('SELECT * from irs_eo_records WHERE EIN = $1;', [ein], function(err, result){
+
       done();
 
-      if(err) {
-        res.json(err);
-        return console.error('error running query', err);
+      if(err){
+        res.status(500).json(err);
+        return;
       }
-      console.log(result.rows[0].number);
-      //output: 1
 
-      res.json(result);
+      if(result.rows.length > 0){
+        res.json(result.rows[0]);
+      }else{
+        res.status(404).json({status: 'Record not found'});
+      }
+
     });
+
+  });
+  // QUERY: /api/records?params....
+  app.get('/api/records', function(req, res){
+
+    var query = req.query;
+
+    // client.query('SELECT * from irs_eo_records;', function(err, result) {
+    //   //call `done()` to release the client back to the pool
+    //   done();
+
+    //   if(err) {
+    //     res.status(500).json(err);
+    //     return console.error('error running query', err);
+    //   }
+    //   console.log(result.rows[0].number);
+    //   //output: 1
+
+    //   res.json(result);
+    // });
 
   });
 
