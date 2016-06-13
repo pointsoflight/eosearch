bc51d62e152f5f4d3a96d07d31cfb723af1e4309
Joseph Reed <atljoereed@gmail.com>
2016-06-13 18:20:26 -0400

    update db setting

--- data_loader/config/settings.py
+++ data_loader/config/settings.py
@@ -1,7 +1,7 @@
 credentials = {
-    'host': 'ec2-23-23-226-24.compute-1.amazonaws.com',
+    'host': 'ec2-54-243-60-62.compute-1.amazonaws.com',
     'port': '5432',
-    'database': 'd3srs2h4brnr26',
-    'user': 'wcxydzuorcheyy',
-    'password': 'Rm-xtzr-j4AnJmuJ7Q7gIKjsl3',
+    'database': 'dcridg3k242kug',
+    'user': 'ardgrvuyquuqwc',
+    'password': 'hQXKf3TErb4nmEpXFB2-fNaggp',
 }
