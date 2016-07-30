1c710e90fea0f59cd3753ff50baf6d19e2caff65
Joseph Reed <atljoereed@gmail.com>
2016-07-30 15:50:54 -0400

    Delete settings.py file
    This is just bugging me leaving these settings out there. Contact me if you need the database settings.

--- data_loader/config/settings.py
+++
@@ -1,7 +0,0 @@
-credentials = {
-    'host': 'ec2-54-243-60-62.compute-1.amazonaws.com',
-    'port': '5432',
-    'database': 'dcridg3k242kug',
-    'user': 'ardgrvuyquuqwc',
-    'password': 'hQXKf3TErb4nmEpXFB2-fNaggp',
-}
